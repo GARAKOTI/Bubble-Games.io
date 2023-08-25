fab80bff632523828ea84b60748ff5e6ad96dd59
GARAKOTI <100749027+GARAKOTI@users.noreply.github.com>
2023-08-25 19:04:56 +0530

    Update script.js
    fixed timer bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var timer = 60;
+var timer = 10;
 var score = 0;
-var hitvalue =0;
+var hitvalue = 0;
 function bubbleMaker() {
   var bubble = "";
   for (var i = 0; i < 105; i++) {
@@ -17,14 +17,14 @@ function runTimer() {
       document.querySelector("#timerValue").textContent = timer;
     } else {
       clearInterval(timerStart);
+      document.querySelector(".pbtm").innerHTML = `<h1>Game Is Over</h1>`;
     }
   }, 1000);
 }
 
-function getHitValue(abc) {
+function getHitValue() {
   hitvalue = Math.floor(Math.random() * 10);
   document.querySelector("#hitValue").textContent = hitvalue;
-  
 }
 
 function increaseScore() {
@@ -32,25 +32,20 @@ function increaseScore() {
   document.querySelector("#scoreValue").textContent = score;
 }
 
-function startTheGame(){
-    document.querySelector(".pbtm").addEventListener("click", function (data) {
-        var clickedNumber =  Number(data.target.textContent);
-       if (timer > 0 && hitvalue == clickedNumber ) {
-           increaseScore();
-           getHitValue();
-         }
-         else if(timer>0 && hitvalue != clickedNumber){
-             alert("OOPS Seems like you clicked the wrong number")
-         }
-          else {
-           alert("OOP seems like timeout");
-         }
-       });
-
+function startTheGame() {
+  document.querySelector(".pbtm").addEventListener("click", function (data) {
+    var clickedNumber = Number(data.target.textContent);
+    if (timer > 0 && hitvalue == clickedNumber) {
+      increaseScore();
+      getHitValue();
+      bubbleMaker();
+    } else if (timer > 0 && hitvalue != clickedNumber) {
+      alert("OOPS Seems like you clicked the wrong number");
+    }
+  });
 }
 
 bubbleMaker();
 runTimer();
 getHitValue();
 startTheGame();
-
